9956283a1c9f458ce85774f24b69bf110debfc5f
humboldtux <humboldtux@gmail.com>
2013-10-14 15:47:57 +0200

    Add title functionality for show page

--- spec/features/creating_activities_spec.rb
+++ spec/features/creating_activities_spec.rb
@@ -2,15 +2,23 @@ require 'spec_helper'
 
 feature 'Creating Activity' do
   scenario "can create an activity" do
+
     visit '/'
 
     click_link 'New Activity'
 
     fill_in 'Name', with: 'Student Project'
     fill_in 'Description', with: 'An activity for student project'
-
     click_button 'Create Activity'
 
     expect(page).to have_content('Activity has been created.')
+    ####
+    activity = Activity.where(name: 'Student Project').first
+
+    expect(page.current_url).to eql(activity_url(activity))
+
+    title = "Student Project - Activities - Shooring"
+    expect(page).to have_title(title)
+
   end
 end
