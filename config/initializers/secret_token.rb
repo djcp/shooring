df5ab025e4e56439417da54c98b6e6a0658cb47f
humboldtux <humboldtux@gmail.com>
2013-10-14 12:29:00 +0200

    initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shooring::Application.config.secret_key_base = '66c69d5c2bfcb5e2c257c08d803fa9d58e31fbfbc27a96f94d4a940a142ff660f7cf347bfaf97321c6625c3b5ddb700b51916c8cbdc3d2d2c30ee54be8a39a85'
