4c9802d99e2e0ca1e54048ffbfad46bdd0f86007
humboldtux <humboldtux@gmail.com>
2013-10-16 17:02:32 +0200

    Implemented signing in.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Shooring::Application.routes.draw do
   root 'activities#index'
 
   resources :users
+  get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
 
   resources :activities do
     resources :folders
