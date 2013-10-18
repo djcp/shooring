d296f86c43c6624123510a90a571aeeaa0a8bb63
humboldtux <humboldtux@gmail.com>
2013-10-18 15:12:45 +0200

    Restrict actions in FoldersController based on permissions and hide links

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -4,6 +4,8 @@ feature "hidden links" do
   let(:user) { FactoryGirl.create(:user) }
   let(:admin) { FactoryGirl.create(:admin_user) }
   let(:activity) { FactoryGirl.create(:activity) }
+  let(:folder) { FactoryGirl.create(:folder, activity: activity,
+                                    user: user) }
 
   context "anonymous users" do
     scenario "cannot see the New Activity link" do
@@ -24,7 +26,7 @@ feature "hidden links" do
 
   context "regular users" do
     before { sign_in_as!(user) }
-      scenario "cannot see the New Activity link" do
+    scenario "cannot see the New Activity link" do
       visit '/'
       assert_no_link_for "New Activity"
     end
@@ -38,6 +40,54 @@ feature "hidden links" do
       visit activity_path(activity)
       assert_no_link_for "Delete Activity"
     end
+
+    scenario "New folder link is shown to a user with permission" do
+      define_permission!(user, "view", activity)
+      define_permission!(user, "create folders", activity)
+      visit activity_path(activity)
+      assert_link_for "New Folder"
+    end
+
+    scenario "New folder link is hidden from a user without permission" do
+      define_permission!(user, "view", activity)
+      visit activity_path(activity)
+      assert_no_link_for "New Folder"
+    end
+
+    scenario "Edit folder link is shown to a user with permission" do
+      folder
+      define_permission!(user, "view", activity)
+      define_permission!(user, "edit folders", activity)
+      visit activity_path(activity)
+      click_link folder.name
+      assert_link_for "Edit Folder"
+    end
+
+    scenario "Edit folder link is hidden from a user without permission" do
+      folder
+      define_permission!(user, "view", activity)
+      visit activity_path(activity)
+      click_link folder.name
+      assert_no_link_for "Edit Folder"
+    end
+
+    scenario "Delete folder link is shown to a user with permission" do
+      folder
+      define_permission!(user, "view", activity)
+      define_permission!(user, "delete folders", activity)
+      visit activity_path(activity)
+      click_link folder.name
+      assert_link_for "Delete Folder"
+    end
+
+    scenario "Delete folder link is hidden from users without permission" do
+      folder
+      define_permission!(user, "view", activity)
+      visit activity_path(activity)
+      click_link folder.name
+      assert_no_link_for "Delete Folder"
+    end
+
   end
 
   context "admin users" do
@@ -55,6 +105,18 @@ feature "hidden links" do
     scenario "can see the Delete Activity link" do
       visit activity_path(activity)
       assert_link_for "Delete Activity"
+    end
+
+    scenario "New folder link is shown to admins" do
+      visit activity_path(activity)
+      assert_link_for "New Folder"
+    end
+
+    scenario "Delete folder link is shown to admins" do
+      folder
+      visit activity_path(activity)
+      click_link folder.name
+      assert_link_for "Delete Folder"
     end
   end
 end
