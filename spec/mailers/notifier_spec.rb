7c9698d237b3c77dd6bd318ffc95c20d60fd8602
Humboldtux <humboldtux@gmail.com>
2013-10-22 22:04:07 +0200

    Notifier is tested

--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -1,5 +1,30 @@
 require "spec_helper"
 
 describe Notifier do
-  #pending "add some examples to (or delete) #{__FILE__}"
+  context "comment_updated" do
+    let!(:activity) { FactoryGirl.create(:activity) }
+    let!(:folder_owner) { FactoryGirl.create(:user) }
+    let!(:folder) { FactoryGirl.create(:folder, activity: activity,
+                                     user: folder_owner) }
+    let!(:commenter) { FactoryGirl.create(:user) }
+    let(:comment) do
+      Comment.new({
+        folder: folder,
+        user: commenter,
+        text: "Test comment"
+        })
+    end
+
+    let(:email) do
+       Notifier.comment_updated(comment, folder_owner)
+    end
+
+    it "sends out an email notification about a new comment" do
+      expect(email.to).to include(folder_owner.email)
+      title = "#{folder.name} for #{activity.name} has been updated."
+      expect(email.body.to_s).to include(title)
+      expect(email.body.to_s).to include("#{comment.user.email} wrote:")
+      expect(email.body.to_s).to include(comment.text)
+    end
+  end
 end
